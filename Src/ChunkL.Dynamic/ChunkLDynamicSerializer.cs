8b00db3f7d8f2fef7eed35c858bc2fdd404d2acd
Petr Pivoňka <petrpiv1@gmail.com>
2024-01-05 19:49:49 +0100

    Add ChunkL.Dynamic

---
+++ Src/ChunkL.Dynamic/ChunkLDynamicSerializer.cs
@@ -0,0 +1,12 @@
+﻿namespace ChunkL.Dynamic;
+
+public static class ChunkLDynamicSerializer
+{
+    public static DynamicClass Deserialize(Stream stream, ChunkLDataModel dataModel)
+    {
+        var header = new HeaderReader(reader).Read();
+        var body = new BodyReader(reader).Read();
+
+        return new ChunkLDataModel(header, body);
+    }
+}
---
+++ Src/ChunkL.Dynamic/DynamicClass.cs
@@ -0,0 +1,7 @@
+﻿namespace ChunkL.Dynamic;
+
+public sealed class DynamicClass
+{
+    public string Name { get; init; }
+    public uint Id { get; init; }
+}
